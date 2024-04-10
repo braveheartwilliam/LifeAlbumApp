40151e6a0c43ae1d7d7a3918724ebb093a1180c7
braveheartwilliam <braveheartwilliam@hotmail.com>
2024-04-10 16:10:25 -0400

    Interim part 2

---
+++ src/routes/pages/travelBlogs/[items]/+page.svelte
@@ -0,0 +1,16 @@
+<script type:module>
+	console.log('Places');
+	import { page } from '$app/stores';
+
+	import Places from '/src/components/Places.svelte';
+
+let serializedItems = $page.params.items;
+
+let items = JSON.parse(decodeURIComponent(serializedItems));
+
+	console.log('places/[items] - crumbs: ', items);
+</script>
+
+<h2>made it to /places/</h2>
+
+<Places {items} />
---
+++ src/routes/pages/trips/[items]/+page.svelte
@@ -0,0 +1,16 @@
+<script type:module>
+	console.log('Diaries');
+	import { page } from '$app/stores';
+
+	import Diaries from '/src/components/Diaries.svelte';
+
+	let serializedItems = $page.params.items;
+
+	let items = JSON.parse(decodeURIComponent(serializedItems));
+
+	console.log('diaries/[items] - crumbs: ', items);
+</script>
+
+<h2>made it to /diaries/</h2>
+
+<Diaries {items} />
